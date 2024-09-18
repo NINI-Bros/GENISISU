b681a7537d7d6a74f66467cb3836edba426d197e
류재준.주임연구원 <ryungom@ezpmp.co.kr>
2024-09-19 00:34:17 +0900

    ✨feat: #14 메인페이지 브레이킹포인트 이전 반응형 작업 (event3)

--- src/components/mainPageComponents/Event3.tsx
+++ src/components/mainPageComponents/Event3.tsx
@@ -8,10 +8,10 @@ export default function Event3 () {
         <h2>GENISISUYEON <span>EVENTS</span></h2>
         <h3>재니시수연에서 진행중인 다양한 이벤트를 소개합니다.</h3>
         <button onClick={(e: React.MouseEvent<HTMLButtonElement>)=>{
-        e.preventDefault();
-        alert('준비중 입니다.')
-      }} 
-      className="mainBtn">VIEW MORE</button>
+            e.preventDefault();
+            alert('준비중 입니다.')
+          }} 
+          className="mainBtn">VIEW MORE</button>
       </article>
     </section>
   )
