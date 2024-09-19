0a17b0189795d880df0a9ab96b8026f01948f37a
류재준.주임연구원 <ryungom@ezpmp.co.kr>
2024-09-19 20:53:16 +0900

    ✨feat: #14 제품상세 버튼위치 이동

--- src/app/(web)/models/[model]/(section)/Section1Index.tsx
+++ src/app/(web)/models/[model]/(section)/Section1Index.tsx
@@ -50,36 +50,40 @@ export default function Section1Index({ modelIndex, modelData, imageArray }: Sec
 
   return (
     <section className="w-screen h-[calc(100vh-100px)] grid grid-cols-[450px_auto] gap-x-[4rem] bg-black z-10">
-      {/* 이미지 영역 */}
       <article className="w-[90%] max-h-full col-start-2 grid-cols-1 flex flex-col justify-items-center mt-[40px] pb-[100px] gap-y-[20px] overflow-hidden justify-between">
+        
+        {/* 이미지 영역 */}
         <figure className="relative aspect-[2/1] overflow-hidden">
           {modelData && <ImageViewer images={imageArray} />}
-          {/* <img src="/images/detail/defaultCar.png" className="object-cover h-[100%] scale-150 " alt="" /> */}
         </figure>
+
+        {/* 가격 표시 */}
         <div className='grid grid-cols-[auto_1fr_1fr] gap-y-[10px] gap-x-[3rem] self-center'>
-          <h2 className="text-[50px] font-Hyundai-sans row-span-2 self-center justify-self-center px-[10px] font-black">{modelName && uppercaseName}</h2>
-          <h3 className="text-[30px] text-[#a4a4a4] col-span-2 justify-self-start self-end">
+          <h2 className="text-[50px] font-Hyundai-sans self-center justify-self-center px-[10px] font-black">{modelName && uppercaseName}</h2>
+          <h3 className="text-[30px] text-[#a4a4a4] col-span-2 self-center">
             시작가격{' '}
             <span className="text-white font-Hyundai-sans">{modelPrice.toLocaleString('ko-KR')}</span>
             <span className="text-[20px] text-white"> 원</span>
           </h3>
-          <div className="flex gap-x-[20px] mt-[0px] col-span-2 self-start">
-            <Link
-              href={{ pathname: '/info', query: { model: modelName } }}
-              className="w-[150px] h-[37px] font-thin border-2 border-white flex items-center justify-center"
-            >
-              시승신청
-            </Link>
-            <button className='mainBtn w-[150px] h-[37px] font-thin border-2 border-white bg-white text-black text-[16px]' style={{fontFamily:"Pretendard"}} onClick={clickNext}>
-              다음
-            </button>
-          </div>
-          <div className='flex justify-self-center col-span-3 mt-[60px] items-end gap-x-[10px]'>
+
+          <div className='flex justify-self-center col-span-3 mt-[0px] items-end gap-x-[10px]'>
             <figure className='w-[30px] h-[30px] relative '>
               <Image src="/images/scrollIcon_g.png" fill sizes='100%' alt="이미지 스크롤 아이콘" className='absolute bg-cover'/> 
             </figure>
             <span className='text-[#999]'>상세내용은 스크롤 해주세요</span>
           </div>
+        </div>
+
+        {/* 이후 레이아웃인 화살표 이동에 대응하는 다음버튼 */}
+        <div className="flex flex-col w-[120px] gap-y-[10px] mt-[0px] col-span-2 self-start absolute top-[620px] left-[80px] ">
+          <Link
+            href={{ pathname: '/info', query: { model: modelName } }}
+            className="w-full h-[37px] font-normal border-2 border-white flex items-center justify-center"
+          >시승신청
+          </Link>
+          <button className='mainBtn w-full h-[37px] font-normal border-2 border-white bg-white text-black text-[16px]' style={{fontFamily:"Pretendard"}} onClick={clickNext}>
+            다음
+          </button>
         </div>
         
       </article>
