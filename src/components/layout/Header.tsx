e1c7c543e0794d28043f9f13fb411ed1c235137b
류재준 <ryungom@gmail.com>
2024-09-23 02:45:00 +0900

    💄style: #14 Header, Footer 반응형 작업

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,20 +3,25 @@
 // import { auth } from "@/auth";
 import Link from 'next/link';
 import { useEffect, useRef, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
 import { signOut, useSession } from 'next-auth/react';
 import Sitemap from './Sitemap';
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseChimney, faCar, faRightToBracket, faHeadphones, faKey } from '@fortawesome/free-solid-svg-icons';
+import { faFileLines } from '@fortawesome/free-regular-svg-icons';
 
 export default function Header({ isMain }: { isMain: string }) {
   // export default async function Header({ isMain }: {isMain:string}) {
   // const session = await auth();
   // console.log('session', session);
   const { data:session, status } =  useSession();
-  const [modelOn, setModelOn] = useState(true)
+  const [modalOn, setmodalOn] = useState(true)
   const [mobileState, setMobileState] = useState({
     mobileView: false,
     thisWidth:0
   })
+  const router = useRouter();
 
   // console.log("모바일 상태 확인",mobileState.mobileView)
 
@@ -29,16 +34,16 @@ export default function Header({ isMain }: { isMain: string }) {
     window.addEventListener('resize',handleResize)
 
     // sitemap 호출
-    if (!modelOn) {
+    if (!modalOn) {
       const scrollY = window.scrollY;
       document.body.style.top = `-${scrollY}px`;
       document.body.style.position = 'fixed';
-      htmlBody.classList.toggle('modalOn')
+
     } else {
       const scrollY = document.body.style.top;
       document.body.style.top = '';
       document.body.style.position = '';
-      htmlBody.classList.toggle('modalOn')
+    
       window.scrollTo(0, parseInt(scrollY || '0') * -1);
     }
 
@@ -55,80 +60,119 @@ export default function Header({ isMain }: { isMain: string }) {
      window.removeEventListener('resize',handleResize)
     )
 
-  },[modelOn, mobileState.thisWidth, mobileState.mobileView])
+  },[modalOn, mobileState.thisWidth, mobileState.mobileView])
 
   const handleChangeSitemapState = (bl : boolean) => {
-    setModelOn(!bl)
+    setmodalOn(!bl)
   }
 
-  const handleSignOut = () => {
+  const handleSignOut = (e: React.MouseEvent) => {
+    e.preventDefault();
     signOut({ callbackUrl: '/' });
   }
   const handleSiteMapOpen = (e: React.MouseEvent<HTMLElement>) : void => {
     e.preventDefault();
-    setModelOn(!modelOn)
+    setmodalOn(!modalOn)
   }
 
   return (
     <header className={isMain}>
-      {!mobileState.mobileView 
-        ? 
-        <ul className="gnb gnb_web">
-          <li>
-            <ul>
-              <li>
-                <h1 className="gnb_logo">
-                  <a href="/">
-                    <img src="/images/genisisu_logo_w.png" alt={'타이틀이미지'} />
-                  </a>
-                </h1>
-              </li>
-              <li>
-                <Link href="/models">모델</Link>
-              </li>
-              <li>
-                <Link href="/drive">전시시승</Link>
+      <nav className="gnb gnb_web">
+        <div className="navWrap">
+          <ul className="firstGnb">
+            <li>
+              <h1 className="gnbLogo">
+                  <Link href="/">
+                    <figure>
+                      <Image src="/images/genisisu_logo_w.png" priority fill sizes="100%" alt={"타이틀이미지"} />
+                    </figure>
+                  </Link>
+              </h1>
+            </li>
+            <li>
+              <Link href="/models">모델</Link>
+            </li>
+            <li>
+              <Link href="/drive">전시시승</Link>
 
-              </li>
-              <li>
-                <Link href="/qna">고객지원</Link>
-              </li>
-              <li>
-                <Link href="/info">공지사항</Link>
-              </li>
-              <li>
-                <Link href="/none">제니시수</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <ul>
-            
-              
-                  <li>
-                    <Link href="/login">로그인</Link>
-                  </li>
-                  <li>
-                    <Link href="/signup">회원가입</Link>
-                  </li>
-                  <li className='cursor-pointer' onClick={handleSignOut}>로그아웃</li>
-              
-          
-              
-              <li>
-                <Link href="#" onClick={(e) => handleSiteMapOpen(e)}>
-                  <img src="/images/menu_ham.png" alt="" />
-                </Link>
-              </li>
-            </ul>
-          </li>
-        </ul>
+            </li>
+            <li>
+              <Link href="/qna">고객지원</Link>
+            </li>
+            <li>
+              <Link href="/info">공지사항</Link>
+            </li>
+            <li>
+              <Link href="/none">제니시수</Link>
+            </li>
+          </ul>
+
+          <ul className="firstGnb mobileView">
+            <li onClick={() => router.push("/models")}>
+              <FontAwesomeIcon icon={faCar} />
+              <span>모델</span>
+            </li>
+            <li onClick={() => router.push("/")}>
+              <FontAwesomeIcon icon={faHouseChimney}/>
+              <span>홈</span>
+            </li>
+            <li onClick={() => router.push("/drive")}>
+              <FontAwesomeIcon icon={faRightToBracket} />
+              <span>전시시승</span>
+            </li>
+            <li onClick={() => router.push("/qna")}>
+              <FontAwesomeIcon icon={faHeadphones} />
+              <span>고객지원</span>
+            </li>
+            <li onClick={() => router.push("/info")}>
+              <FontAwesomeIcon icon={faFileLines} />
+              <span>공지사항</span>
+            </li>
+          </ul>
+
+        </div>
+        <div className="navWrap">
+          <ul className="secondGnb">
+  
+            <li>
+              <Link href="/login">로그인</Link>
+              <Link href="/signup">회원가입</Link>
+              <Link href="#" className="cursor-pointer" onClick={e => handleSignOut(e)}>로그아웃</Link>
+            </li>
+
+            <li className="sitemapBtn">
+              <Link href="#" onClick={(e) => handleSiteMapOpen(e)}>
+                <figure>
+                  <Image src="/images/menu_ham.png" fill sizes='100%' alt="" />
+                </figure>
+              </Link>
+            </li>
+
+          </ul>
+
+          <ul className="mobileView">
+            <li onClick={() => router.push("/login")}>
+              <span>로그인</span>
+            </li>
+            <li onClick={() => router.push("/signup")}>
+              <span>회원가입</span>
+            </li>
+            {/* 로그인여부에 따라 다르게 호출 */}
+            <li onClick={e => handleSignOut(e)}>
+              <span>로그아웃</span>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+      {/* {!mobileState.mobileView 
+        ? 
         : ""
-      }
+      } */}
 
 
       {/* 사이트맵 컴포넌트 */}
-      <Sitemap modalState={modelOn} modalToggleFn={setModelOn}/>
+      <Sitemap modalState={modalOn} modalToggleFn={setmodalOn}/>
 
   </header>
   );
