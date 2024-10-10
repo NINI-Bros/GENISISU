a072158e6c2a92ef6a0068b23c3fc89991e57a34
류재준 <ryungom@gmail.com>
2024-10-10 18:35:18 +0900

    💄style : #14 결제완료페이지 반응형 작업 완료

--- src/app/(web)/models/paymentsComplete/page.tsx
+++ src/app/(web)/models/paymentsComplete/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 const initialCart = {
@@ -31,22 +32,22 @@ export default function PaymentsComplete() {
       {/* <figure className="relative w-[80px] h-[80px] mb-[-25px]">
           <Image src="/images/warning.png" fill style={{objectFit:"contain"}} className="absolute top-0 left-0" alt="잘못된 페이지를 들어왔을때 보여주는 경고이미지"></Image>
         </figure> */}
-      <div className="h-[calc(100vh-420px)] bg-black text-white p-4 flex flex-col items-center justify-center gap-y-[40px]">
+      <div className="h-[calc(100vh-420px)] bg-black text-white p-4 flex flex-col items-center justify-center gap-y-[40px] max-[1366px]:h-screen">
         <div className="flex flex-col">
-          <h1 className="text-[60px] text-center text-white border-b-[1px] border-white">
+          <h1 className="text-[60px] text-center text-white border-b-[1px] border-white max-[1366px]:text-[32px]">
             {title || ''}{' '}
           </h1>
-          <h2 className="text-[40px] text-center mt-[0px]">결제에 성공했습니다!</h2>
+          <h2 className="text-[40px] text-center mt-[0px] max-[1366px]:text-[30px]">결제에 성공했습니다!</h2>
         </div>
         <div>
-          <h3 className="text-[20px] mb-2 text-right">
-            <span className="text-[25px text-left">구입 가격 : </span>
+          <h3 className="text-[20px] mb-2 text-right ">
+            <span className="text-[25px] text-left max-[1366px]:text-[20px]">구입 가격 : </span>
             {price.toLocaleString() || ''}원
           </h3>
         </div>
-        <button className="btnBasic border-[white] px-[40px] py-[15px] mt-[40px] hover:bg-white hover:text-black">
+        <Link href="/" className="btnBasic border-[white] px-[40px] py-[15px] mt-[40px] hover:bg-white hover:text-black">
           홈으로 돌아가기
-        </button>
+        </Link>
       </div>
     </main>
   );
