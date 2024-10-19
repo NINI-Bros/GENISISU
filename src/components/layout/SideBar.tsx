8607e61456102aa0143435fa02be4af3c71b9cc5
류재준 <ryungom@gmail.com>
2024-10-19 16:51:55 +0900

    ✨ feat: #53 sideGnb 기능개발 완료

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,16 +1,21 @@
-import { fetchVehicles } from "@/data/fetch/productFetch";
+"use client";
+
+import { fetchOption, fetchVehicles } from "@/data/fetch/productFetch";
 import { useSession } from "@/hook/session";
 import useModalOpenBgFix from "@/hook/useModalOpenBgFix";
+import { OptionList } from "@/types/optionLayout";
 import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation"
+import { useParams, usePathname, useRouter } from "next/navigation"
 import { useEffect, useRef, useState } from "react"
 
 export default function SideBar () {
   const route = useRouter();
   const path = usePathname();
+  console.log("패스확인",path.split('/'))
+  const param = useParams();
   const session = useSession();
   const mobileSideBarRef = useRef<HTMLDivElement | null>(null)
   const toggleModelRef = useRef<HTMLDivElement | null>(null)
@@ -18,6 +23,20 @@ export default function SideBar () {
   const arrowBtnRef = useRef<HTMLElement | null>(null)
   const arrowBtnRef_2 = useRef<HTMLElement | null>(null)
   const [modalOn, setModalOn] = useState(false);
+
+  const optionList: OptionList = {
+    detail: '모델 상세',
+    engine: '엔진 타입',
+    drivetrain: '구동 타입',
+    passenger: '시트 구성',
+    exterior: '외장 컬러',
+    interior: '내장디자인 & 컬러',
+    garnish: '내장가니쉬',
+    wheel: '휠 & 타이어',
+    add: '선택 품목',
+    payments: '결제',
+  };
+  type OptionKey = keyof typeof optionList;
 
   const handleSignOut = (e: React.MouseEvent) => {
     e.preventDefault();
@@ -64,21 +83,44 @@ export default function SideBar () {
     vehicleData();
   },[])
 
+
+  // 모델별 옵션내용 표기 함수
+  const optionViewNames = () => {
+    
+    const removeClassOn = () => {mobileSideBarRef.current?.classList.remove('on')}
+    
+    if (param?.model !== undefined) {
+      return Object.keys(optionList).map((optionName) => (
+        <>
+          <Link 
+            key={optionName + '_key'} 
+            href={`/models/${param.model}/${optionName === 'detail' ? '' : optionName}`}
+            onClick={removeClassOn}
+            className={optionName === param.option ? 'text-white' : 'text-[#666]'}
+          >{optionList[optionName]}</Link>
+        </>
+      ))
+    } else {
+      return
+    }
+  }
+
+ 
+
   // 모델명 컴포넌트
   const MenuList = () => {
     return (
       <>
         {titdata.map((item,i) => (
           item !== "NEOLUN CONCEPT" ? (
-            <>
-              <Link key={'model_' + (i + 1)} href={'/models/' + (i + 1)} >
+            <section key={'model_' + (i + 1)} >
+              <Link href={'/models/' + (i + 1)}>
                 {item}
-                <h4>1</h4>
-                <h4>2</h4>
-                <h4>3</h4>
-                <h4>4</h4>
               </Link>
-            </>
+              <div className="toggleModelOptions flex flex-col pl-4">
+                {i + 1 === Number(param.model) ? optionViewNames() : ''}
+              </div>
+            </section>
           ) : (
             <Link key={'model_' + 13 } href='#' onClick={(e) => {
               e.preventDefault();
