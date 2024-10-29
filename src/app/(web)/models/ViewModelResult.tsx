c3f43b7622a56b694221fbbcb24fd40c3edfdac1
류재준 <ryungom@gmail.com>
2024-10-29 20:56:24 +0900

    🐛fix: #79 통합회원가입 피드백 2차 수정수정

--- src/app/(web)/models/ViewModelResult.tsx
+++ src/app/(web)/models/ViewModelResult.tsx
@@ -3,7 +3,8 @@
 import Button from '@/components/Button';
 import ModelCard from './ModelCard';
 import { Product, ViewModelCards } from '@/types/product';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import ScrollToTop from '@/components/ScrollToTop';
 
 export default function ViewModelResult({ data }: { data: Product[] }) {
   const [value, setValue] = useState<ViewModelCards>({
@@ -11,15 +12,6 @@ export default function ViewModelResult({ data }: { data: Product[] }) {
     searchActiveValue: '', // 검색 input값
     modelJsx: [],
   });
-
-  useEffect(() => {
-    const { resultData } = productComponent(value.searchActiveValue);
-    setValue((prev) => ({
-      ...prev,
-      modelJsx: resultData,
-    }));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value.searchActiveValue]);
 
   // 검색input 작성후 엔터값 지정
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -49,9 +41,12 @@ export default function ViewModelResult({ data }: { data: Product[] }) {
 
   return (
     <>
+      <ScrollToTop />
       <div className="flex justify-between mb-[20px] items-center max-[600px]:flex-col max-[600px]:gap-y-[20px]">
         <span className="text-white max-[600px]:self-end">
-          {value.modelJsx.length > 0 ? `총 ${value.modelJsx.length}개 모델` : ''}
+          {productComponent(value.searchActiveValue).resultData.length > 0
+            ? `총 ${productComponent(value.searchActiveValue).resultData.length}개 모델`
+            : ''}
         </span>
         <div className="flex justify-end min-h-[45px] max-[600px]:w-full">
           <input
@@ -71,16 +66,17 @@ export default function ViewModelResult({ data }: { data: Product[] }) {
           <Button
             className="btnBasic min-w-[80px] border-[1px] border-white ml-[5%]"
             onClick={() => {
-              setValue((prev) => ({ ...prev, searchActiveValue: '' }));
+              setValue((prev) => ({ ...prev, inputValue: '', searchActiveValue: '' }));
             }}
           >
             초기화
           </Button>
         </div>
       </div>
-      {value.modelJsx.length > 0 ? (
+      {productComponent(value.searchActiveValue).resultData.length > 0 ? (
         <ul className="grid grid-cols-5 max-[1920px]:grid-cols-4 max-[1366px]:grid-cols-3 max-[890px]:grid-cols-2 max-[600px]:grid-cols-1 gap-6 text-white">
-          {value.modelJsx}
+          {/* {value.modelJsx} */}
+          {productComponent(value.searchActiveValue).resultData}
         </ul>
       ) : (
         <ul className="h-full flex justify-center items-center">
