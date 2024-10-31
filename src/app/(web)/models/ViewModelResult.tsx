45c6f056a6afdafac59dcb6b362b0050614671a8
류재준 <ryungom@gmail.com>
2024-10-31 11:17:32 +0900

    🐛fix: #79 모델결과값 초기 마운트 코드 정리

--- src/app/(web)/models/ViewModelResult.tsx
+++ src/app/(web)/models/ViewModelResult.tsx
@@ -10,7 +10,6 @@ export default function ViewModelResult({ data }: { data: Product[] }) {
   const [value, setValue] = useState<ViewModelCards>({
     inputValue: '', // 초기 input값 세팅
     searchActiveValue: '', // 검색 input값
-    modelJsx: [],
   });
 
   // 검색input 작성후 엔터값 지정
@@ -31,12 +30,11 @@ export default function ViewModelResult({ data }: { data: Product[] }) {
 
   // 결과값 모델 호출
   const productComponent = (modelName: string) => {
-    const resultData = data
+    return data
       .filter((item) => item.name.includes(modelName))
       .map((model, index) => {
         return <ModelCard key={index} model={model} />;
       });
-    return { resultData };
   };
 
   return (
@@ -44,8 +42,8 @@ export default function ViewModelResult({ data }: { data: Product[] }) {
       <ScrollToTop />
       <div className="flex justify-between mb-[20px] items-center max-[600px]:flex-col max-[600px]:gap-y-[20px]">
         <span className="text-white max-[600px]:self-end">
-          {productComponent(value.searchActiveValue).resultData.length > 0
-            ? `총 ${productComponent(value.searchActiveValue).resultData.length}개 모델`
+          {productComponent(value.searchActiveValue).length > 0
+            ? `총 ${productComponent(value.searchActiveValue).length}개 모델`
             : ''}
         </span>
         <div className="flex justify-end min-h-[45px] max-[600px]:w-full">
@@ -73,10 +71,9 @@ export default function ViewModelResult({ data }: { data: Product[] }) {
           </Button>
         </div>
       </div>
-      {productComponent(value.searchActiveValue).resultData.length > 0 ? (
+      {productComponent(value.searchActiveValue).length > 0 ? (
         <ul className="grid grid-cols-5 max-[1920px]:grid-cols-4 max-[1366px]:grid-cols-3 max-[890px]:grid-cols-2 max-[600px]:grid-cols-1 gap-6 text-white">
-          {/* {value.modelJsx} */}
-          {productComponent(value.searchActiveValue).resultData}
+          {productComponent(value.searchActiveValue)}
         </ul>
       ) : (
         <ul className="h-full flex justify-center items-center">
