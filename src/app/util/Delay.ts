2b20229e9d915e5d5283319cad1609bc9dba7090
류재준 <ryungom@gmail.com>
2025-03-17 20:46:46 +0900

    🐛fix: #91 delay 함수의 함수명, 코드길이, 함수호출부분 수정

--- src/app/util/Delay.ts
+++ src/app/util/Delay.ts
@@ -1,8 +1,2 @@
-// eslint-disable-next-line require-await
-export default async function DelayTests(count: number) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('');
-    }, count);
-  });
-}
+export const delayTests = async (count: number) =>
+  new Promise((resolve) => setTimeout(resolve, count));
